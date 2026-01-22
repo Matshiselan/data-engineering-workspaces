886b63e77f800785eb01acef99a32b84c8c21c3c
Ntandoyenkosi Matshisela <matshiselan@gmail.com>
2026-01-22 12:04:51 +0000

    pipeline code working

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,6 +1,11 @@
-import sys
 import pandas as pd
+import sys
 
+# Create a sample DataFrame
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
 
-
-print('hello pipeline')
+# Save DataFrame to a Parquet file with dynamic filename based on command-line argument
+output_path = f"/app/output_day_{sys.argv[1]}.parquet"
+print(f"Saving to {output_path}")
+df.to_parquet(output_path)
